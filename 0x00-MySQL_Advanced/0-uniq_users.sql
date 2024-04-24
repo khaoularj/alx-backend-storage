ac709280bc7aac1d6285e4d812a318191996c47c
khaoularj <khaoula.raja1@gmail.com>
2024-04-24 18:59:27 +0000

    create users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,11 @@
 --this script creates a table users
+--With these attributes:
+--id, integer, never null, auto increment and primary key
+--email, string (255 characters), never null and unique
+--name, string (255 characters)
+
 CREATE TABLE IF NOT EXISTS users (
-    id NOT NULL INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
