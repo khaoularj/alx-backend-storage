2a4a470351fa26c39519b79a2dcfb652715221b1
khaoularj <khaoula.raja1@gmail.com>
2024-04-25 14:06:56 +0000

    task 0_ mysql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,3 @@
---this script creates a table users
---With these attributes:
---id, integer, never null, auto increment and primary key
---email, string (255 characters), never null and unique
---name, string (255 characters)
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
