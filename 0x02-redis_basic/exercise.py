13c283ee26a12bbe1585f35687f260f1202df5db
khaoularj <khaoula.raja1@gmail.com>
2024-04-30 09:57:55 -0700

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,13 +19,28 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """decorator that takes a single method
+    Callable argument and returns a Callable"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_k = f"{method.__qualname__}:inputs"
+        output_k = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_k, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(output_k, res)
+        return res
+    return wrapper
+
+
 class Cache:
     """this is theclass Cache"""
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """this is store method that
         that takes a data argument and returns a string"""
@@ -57,8 +72,16 @@ class Cache:
 
 if __name__ == "__main__":
     cache = Cache()
-    cache.store(b"first")
-    print(cache.get(cache.store.__qualname__))
-    cache.store(b"second")
-    cache.store(b"third")
-    print(cache.get(cache.store.__qualname__))
+
+    s1 = cache.store("first")
+    print(s1)
+    s2 = cache.store("second")
+    print(s2)
+    s3 = cache.store("third")
+    print(s3)
+
+    inputs = cache._redis.lrange(f"{cache.store.__qualname__}:inputs", 0, -1)
+    outputs = cache._redis.lrange(f"{cache.store.__qualname__}:outputs", 0, -1)
+
+    print("inputs:", inputs)
+    print("outputs:", outputs)
