9ea89033944dda172bf1d2239d6366ad80f808fe
khaoularj <khaoula.raja1@gmail.com>
2024-04-30 09:13:19 -0700

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 import uuid
 from typing import Union
+from typing import Callable
 
 
 class Cache:
@@ -18,8 +19,26 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
+        """this is get method that take a key string
+        argument and an optional Callable argument named fn"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """get_str method"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """get_int method"""
+        return self.get(key, fn=int)
+
 
 if __name__ == "__main__":
+"""    
     cache = Cache()
 
     data = b"hello"
@@ -28,3 +47,15 @@ if __name__ == "__main__":
 
     local_redis = redis.Redis()
     print(local_redis.get(key))
+"""
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
